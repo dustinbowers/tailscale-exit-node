54c73b53cd44f4ed9f02818b8565b6b16f639a1f
Dustin Bowers <dustin.uncc@gmail.com>
2024-09-24 20:22:05 -0400

    Add start/stop scripts for a Tailscale exit node

--- run_tailscale.sh
+++
@@ -1,7 +0,0 @@
-docker run --name=tailscale \
-  --rm \
-  --privileged \
-  -v tailscale-state:/var/lib/tailscale \
-  -p 41641:41641 \
-  -p 10000-20000:10000-20000/udp \
-  tailscale/tailscale tailscaled
